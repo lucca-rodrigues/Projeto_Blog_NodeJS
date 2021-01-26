389e6b1f6e083c573136183b7835cee4af126a88
lucca-rodrigues <contato.luccarodrigues@gmail.com>
2021-01-26 16:15:57 -0300

    add form send categories

--- Controllers/ControllerCategories.js
+++ Controllers/ControllerCategories.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const Categories = require("../Models/ModelCategories");
+const slugify = require("slugify");
 
 router.get("/new", (req, res) => {
     res.render("Pages/Categories/New", );
+})
+
+router.post("/new", (req, res) => {
+    const title = req.body.title;
+
+    if(title != undefined){
+        Categories.create({title, slug: slugify(title)})
+        .then(() => {
+            res.redirect("/");
+        })
+    }else{
+        res.redirect("/categories/new");
+    }
 })
 
 
