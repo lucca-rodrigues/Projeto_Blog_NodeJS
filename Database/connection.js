f22aec80bfbb63d54e633804e67954a784acd1b4
Lucca Rodrigues <lucasrod2008rigues@gmail.com>
2021-01-26 16:39:14 -0300

    Delete connection.js

--- Database/connection.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require("sequelize");
-
-const connection = new Sequelize('projetoblog', 'root', 'Shopboyz1!',{
-    host: 'localhost',
-    dialect: 'mysql'
-});
-
-module.exports = connection;
